a6c8bfb58806334adccf0c7ceef70c2a4956024f
Alex Kachak <kachak2016@mail.ru>
2023-05-20 12:38:03 +0700

    ghm

--- libs/components/database.py
+++ libs/components/database.py
@@ -1,12 +1,12 @@
-# import sqlite3
+import sqlite3
 
-# db = sqlite3.connect('server.db')
-# sql = db.cursor()
+db = sqlite3.connect('server.db')
+sql = db.cursor()
 
-# sql.execute("""CREATE TABLE IF NOT EXISTS users (
-#     login TEXT,
-#     password TEXT
-# )
-# """)
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    login TEXT,
+    password TEXT
+)
+""")
 
-# db.commit()
+db.commit()
--- libs/screens/formsManager.py
+++ libs/screens/formsManager.py
@@ -1,14 +1,33 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager
 from libs.screens.tabs import DownPanel
-# from libs.components.database import db
+from libs.components.database import db, sql
 
 class InForm(Screen):
     def check(self):
-        return True
+        log = self.inLogin.text
+        pas = self.inPassword.text
+        for value in sql.execute("SELECT * FROM users"):
+            if((value[0]==log) and (value[1]==pas)):
+                return True
+            else:
+                return False
 
 class RegForm(Screen):
-    pass
+    def reg(self):
+        log= self.regLogin.text
+        pas= self.regPassword.text
+        sql.execute("SELECT login FROM users")
+        if sql.fetchone() is None:
+            if(pas==self.regRetPassword.text):
+                sql.execute(f"INSERT INTO users VALUES(?,?)",(log, pas))
+                db.commit()
+                return True
+            else:
+                return False
+        else:
+            return False
+
 
 class WTFForm(Screen):
     pass
